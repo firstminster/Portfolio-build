219b921cb7f69fb3581c823310e73396e310bda0
Promise Harry <chinecherempharry@gmail.com>
2021-02-19 23:45:03 +0100

    form setup with netlify

--- src/pages/Contact/Form.js
+++ src/pages/Contact/Form.js
@@ -2,17 +2,33 @@ import React from 'react'
 
 const Form = () => {
   return (
-    <form>
-      <input type='text' id='name' placeholder='Name' />
-      <input type='text' id='email' placeholder='Email' />
-      <input type='text' id='subject' placeholder='Subject' />
+    <form action='POST' data-netlify='true'>
+      <input type='text' name='name' id='name' placeholder='Name' required />
+      <input type='text' name='email' id='email' placeholder='Email' required />
+      <input
+        type='text'
+        name='subject'
+        id='subject'
+        placeholder='Subject'
+        required
+      />
       <textarea
-        name='#'
+        name='message'
         id='message'
         cols='30'
         rows='10'
         placeholder='Message'
+        required
       ></textarea>
+      <input
+        type='file'
+        name='myfile'
+        id='myfile'
+        placeholder='Upload File'
+        rows='7'
+        style={{ border: 'none' }}
+      />
+      <div className='' data-netlify-recaptcha='true'></div>
       <div className='btn-form'>
         <button type='submit' className='btn btn-primary'>
           Send

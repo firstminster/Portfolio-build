789099342c40264575998aaadc93f2fce25e09b8
Promise Harry <chinecherempharry@gmail.com>
2021-02-20 04:40:55 +0100

    netlify form setup

--- src/pages/Contact/Form.js
+++ src/pages/Contact/Form.js
@@ -1,47 +1,13 @@
 /* eslint-disable no-undef */
 import React from 'react'
-// import Recaptcha from 'react-google-recaptcha'
-
-// const RECAPTCHA_KEY = '6LePqF8aAAAAAJuZH3UUGdBB8UmIILvRALvF8ytv'
 
 const Form = () => {
-  // const [state, setState] = React.useState({})
-
-  // const recaptchaRef = React.createRef() // new Ref for reCaptcha
-
-  // // With this the button is disabled by default, but on Recaptcha change, the form can be submitted
-  // const [buttonDisabled, setButtonDisabled] = React.useState(true)
-
-  // const handleChange = e => {
-  //   setState({ ...state, [e.target.name]: e.target.value })
-  // }
-
-  // const handleSubmit = e => {
-  //   e.preventDefault()
-  //   const form = e.target
-  //   const recaptchaValue = recaptchaRef.current.getValue()
-
-  //   fetch('/', {
-  //     method: 'POST',
-  //     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-  //     body: encode({
-  //       'form-name': form.getAttribute('name'),
-  //       'g-recaptcha-response': recaptchaValue,
-  //       ...state,
-  //     }),
-  //   })
-  //     .then(() => form.getAttribute('action'))
-  //     .catch(error => alert(error))
-  // }
-
   return (
     <form
       name='contact-form'
       method='POST'
-      // data-netlify-recaptcha='true'
       data-netlify='true'
       action='/contact'
-      // onSubmit={handleSubmit}
     >
       <input type='hidden' name='form-name' value='contact-form' />
       <input
@@ -49,7 +15,6 @@ const Form = () => {
         name='name'
         id='name-input'
         placeholder='Name'
-        // onChange={handleChange}
         required
       />
       <input
@@ -57,7 +22,6 @@ const Form = () => {
         name='email'
         id='email-input'
         placeholder='Email'
-        // onChange={handleChange}
         required
       />
       <input
@@ -65,7 +29,6 @@ const Form = () => {
         name='subject'
         id='subject-input'
         placeholder='Subject'
-        // onChange={handleChange}
         required
       />
       <textarea
@@ -74,34 +37,11 @@ const Form = () => {
         cols='30'
         rows='10'
         placeholder='Message'
-        // onChange={handleChange}
         required
       ></textarea>
-      <input
-        // className='form__file'
-        type='file'
-        name='myfile'
-        id='myfile-input'
-        placeholder='Upload File'
-        rows='7'
-        style={{ border: 'none' }}
-        // onChange={handleChange}
-      />
-      {/* <div data-netlify-recaptcha='true'></div> */}
-      {/* Recaptcha */}
-      {/* <Recaptcha
-        ref={recaptchaRef}
-        sitekey={RECAPTCHA_KEY}
-        size='normal'
-        id='recaptcha-google'
-        onChange={() => setButtonDisabled(false)}
-      /> */}
+
       <div className='btn-form'>
-        <button
-          type='submit'
-          className='btn btn-primary'
-          // disabled={buttonDisabled}
-        >
+        <button type='submit' className='btn btn-primary'>
           Send
         </button>
       </div>

74eecc833b60bea4cc670dcfe22197e12d9cb6c8
Promise Harry <chinecherempharry@gmail.com>
2021-02-20 03:19:41 +0100

    added recaptcha

--- src/pages/Contact/Form.js
+++ src/pages/Contact/Form.js
@@ -1,38 +1,38 @@
 /* eslint-disable no-undef */
 import React from 'react'
-import Recaptcha from 'react-google-recaptcha'
+// import Recaptcha from 'react-google-recaptcha'
 
-const RECAPTCHA_KEY = '6LePqF8aAAAAAJuZH3UUGdBB8UmIILvRALvF8ytv'
+// const RECAPTCHA_KEY = '6LePqF8aAAAAAJuZH3UUGdBB8UmIILvRALvF8ytv'
 
 const Form = () => {
-  const [state, setState] = React.useState({})
+  // const [state, setState] = React.useState({})
 
-  const recaptchaRef = React.createRef() // new Ref for reCaptcha
+  // const recaptchaRef = React.createRef() // new Ref for reCaptcha
 
-  // With this the button is disabled by default, but on Recaptcha change, the form can be submitted
-  const [buttonDisabled, setButtonDisabled] = React.useState(true)
+  // // With this the button is disabled by default, but on Recaptcha change, the form can be submitted
+  // const [buttonDisabled, setButtonDisabled] = React.useState(true)
 
-  const handleChange = e => {
-    setState({ ...state, [e.target.name]: e.target.value })
-  }
+  // const handleChange = e => {
+  //   setState({ ...state, [e.target.name]: e.target.value })
+  // }
 
-  const handleSubmit = e => {
-    e.preventDefault()
-    const form = e.target
-    const recaptchaValue = recaptchaRef.current.getValue()
+  // const handleSubmit = e => {
+  //   e.preventDefault()
+  //   const form = e.target
+  //   const recaptchaValue = recaptchaRef.current.getValue()
 
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({
-        'form-name': form.getAttribute('name'),
-        'g-recaptcha-response': recaptchaValue,
-        ...state,
-      }),
-    })
-      .then(() => form.getAttribute('action'))
-      .catch(error => alert(error))
-  }
+  //   fetch('/', {
+  //     method: 'POST',
+  //     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+  //     body: encode({
+  //       'form-name': form.getAttribute('name'),
+  //       'g-recaptcha-response': recaptchaValue,
+  //       ...state,
+  //     }),
+  //   })
+  //     .then(() => form.getAttribute('action'))
+  //     .catch(error => alert(error))
+  // }
 
   return (
     <form
@@ -40,15 +40,15 @@ const Form = () => {
       method='POST'
       data-netlify-recaptcha='true'
       data-netlify='true'
-      action='POST'
-      onSubmit={handleSubmit}
+      // action='POST'
+      // onSubmit={handleSubmit}
     >
       <input
         type='text'
         name='name'
         id='name-input'
         placeholder='Name'
-        onChange={handleChange}
+        // onChange={handleChange}
         required
       />
       <input
@@ -56,7 +56,7 @@ const Form = () => {
         name='email'
         id='email-input'
         placeholder='Email'
-        onChange={handleChange}
+        // onChange={handleChange}
         required
       />
       <input
@@ -64,7 +64,7 @@ const Form = () => {
         name='subject'
         id='subject-input'
         placeholder='Subject'
-        onChange={handleChange}
+        // onChange={handleChange}
         required
       />
       <textarea
@@ -73,7 +73,7 @@ const Form = () => {
         cols='30'
         rows='10'
         placeholder='Message'
-        onChange={handleChange}
+        // onChange={handleChange}
         required
       ></textarea>
       <input
@@ -84,17 +84,17 @@ const Form = () => {
         placeholder='Upload File'
         rows='7'
         style={{ border: 'none' }}
-        onChange={handleChange}
+        // onChange={handleChange}
       />
-
+      <div data-netlify-recaptcha='true'></div>
       {/* Recaptcha */}
-      <Recaptcha
+      {/* <Recaptcha
         ref={recaptchaRef}
         sitekey={RECAPTCHA_KEY}
         size='normal'
         id='recaptcha-google'
         onChange={() => setButtonDisabled(false)}
-      />
+      /> */}
       <div className='btn-form'>
         <button
           type='submit'

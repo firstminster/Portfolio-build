9166ff3c60046efc9b4baceb4619f002fe963d4b
Promise Harry <chinecherempharry@gmail.com>
2021-02-21 14:59:59 +0100

    made the name in my home page bold

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -27,10 +27,11 @@ const Home = () => {
           <div className='home__brief'>
             <h1>Welcome</h1>
             <p className='home__brief-text'>
-              Hello! Chinecherem Harry here, i'm a Software Developer based in
-              Sweden who loves building responsive and user friendly web apps
-              and have a strong knowledge about the backend technologies. With
-              every line of code, I strive to make the web a beautiful place.
+              Hello! <strong> Chinecherem Harry</strong> here, i'm a Software
+              Developer based in Sweden who loves building responsive and user
+              friendly web apps and have a strong knowledge about the backend
+              technologies. With every line of code, I strive to make the web a
+              beautiful place.
               <span className='readmore-icon'>
                 <Link to='/profile' className='home__brief-link'>
                   Read More <i className='fas fa-chevron-right'></i>

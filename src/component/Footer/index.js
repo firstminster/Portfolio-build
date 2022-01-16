fd2b66c4aa499a08f81f204c8cd525bf6763a999
Promise Harry <chinecherempharry@gmail.com>
2022-01-16 23:17:25 +0100

    modified portfolio page

--- src/component/Footer/index.js
+++ src/component/Footer/index.js
@@ -1,18 +1,18 @@
-import React from 'react'
+import React from "react";
 
-import './Footer.scss'
+import "./Footer.scss";
 
 const index = () => {
   return (
-    <footer className='footer'>
-      <p className='footer__text'>
-        Created with{' '}
-        <img src='./images/heart.png' alt='heart' className='footer__heart' />{' '}
+    <footer className="footer">
+      <p className="footer__text">
+        Developed with{" "}
+        <img src="./images/heart.png" alt="heart" className="footer__heart" />{" "}
         Firstminster {new Date().getFullYear()}
         &copy;
       </p>
     </footer>
-  )
-}
+  );
+};
 
-export default index
+export default index;
